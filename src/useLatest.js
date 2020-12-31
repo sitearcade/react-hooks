e280348a6f06aa945bfa2f80fa390144487a144f
Mike Stop Continues <mikestopcontinues@gmail.com>
2020-12-31 14:43:01 -0600

    Add useLatestState

--- src/useLatest.js
+++ src/useLatest.js
@@ -1,13 +1,21 @@
 // import
 
-import {useRef, useEffect} from 'react';
+import {useRef, useEffect, useState} from 'react';
 
 // export
 
-export function useLatestRef(val) {
+export function useLatestRef(val, deps) {
   const ref = useRef(val);
 
-  useEffect(() => (ref.current = val));
+  useEffect(() => (ref.current = val), deps);
 
   return ref;
+}
+
+export function useLatestState(stateFn, deps) {
+  const [state, setState] = useState(stateFn);
+
+  useEffect(() => setState(stateFn()), deps);
+
+  return state;
 }
